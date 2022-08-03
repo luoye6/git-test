9528a740fc68f9e100e1c6fa6f33632a57eaa890
xiaobaitiao <909088445@qq.com>
2022-08-03 13:54:32 +0800

    second commit

--- src/main/java/com/aiguigu/git/TestGit.java
+++ src/main/java/com/aiguigu/git/TestGit.java
@@ -3,5 +3,6 @@ package com.aiguigu.git;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello Git !");
+        System.out.println("Hello Git 2");
     }
 }
